e920eb068d66fde0a9c57dc1166234d0a3e68a42
PedroHS137 <pjherreras@hotmail.com>
2020-03-30 19:24:50 -0600

    modified files

--- ALUControl.v
+++ ALUControl.v
@@ -20,12 +20,23 @@ module ALUControl
 
 );
 
-localparam R_Type_AND    = 9'b111_100100;
-localparam R_Type_OR     = 9'b111_100101;
-localparam R_Type_NOR    = 9'b111_100111;
-localparam R_Type_ADD    = 9'b111_100000;
-localparam I_Type_ADDI   = 9'b100_xxxxxx;
-localparam I_Type_ORI    = 9'b101_xxxxxx;
+localparam R_Type_ADD   = 10'b1111_100000;
+localparam R_Type_JR	= 10'b1111_001000;
+localparam R_Type_SUB	= 10'b1111_100010;
+localparam R_Type_OR    = 10'b1111_100101;
+localparam R_Type_NOR   = 10'b1111_100111; 
+localparam R_Type_SLL 	= 10'b1111_000000;
+localparam R_Type_SRL	= 10'b1111_000010;
+localparam R_Type_AND   = 10'b1111_100100;
+localparam I_Type_ANDI  = 10'b1100_xxxxxx;
+localparam I_Type_LUI	= 10'b0010_xxxxxx;
+localparam I_Type_ADDI  = 10'b1000_xxxxxx;
+localparam I_Type_LW	= 10'b1110_xxxxxx;
+localparam I_Type_SW	= 10'b0110_xxxxxx;
+localparam I_Type_BEQ	= 10'b0100_xxxxxx;
+localparam I_Type_ORI   = 10'b1010_xxxxxx; 
+localparam I_Type_BNE	= 10'b0111_xxxxxx;
+
 
 reg [3:0] ALUControlValues;
 wire [8:0] Selector;
@@ -34,12 +45,25 @@ assign Selector = {ALUOp, ALUFunction};
 
 always@(Selector)begin
 	casex(Selector)
-		R_Type_AND:    ALUControlValues = 4'b0000;
-		R_Type_OR: 		ALUControlValues = 4'b0001;
+		R_Type_AND: ALUControlValues = 4'b0000;
+		R_Type_NOR:	ALUControlValues = 4'b0101;
+		R_Type_SLL: ALUControlValues = 4'b0110;
+		R_Type_SRL:	ALUControlValues = 4'b0111;
+		R_Type_JR: 	ALUControlValues = 4'b1001;
+		R_Type_OR: 	ALUControlValues = 4'b0001;
+		R_Type_ADD:	ALUControlValues = 4'b0000;
+		R_Type_SUB:	ALUControlValues = 4'b0001;
+		I_Type_ADDI:ALUControlValues = 4'b0000;
+		I_Type_ANDI:ALUControlValues = 4'b0011;
+		I_Type_LUI:	ALUControlValues = 4'b0100;
+		I_Type_ORI:	ALUControlValues = 4'b0010;
+		I_Type_SW:	ALUControlValues = 4'b0000;
+		I_Type_LW:	ALUControlValues = 4'b0000;
+		I_Type_BEQ:	ALUControlValues = 4'b1000;
+		I_Type_BNE:	ALUControlValues = 4'b1000;
 		
-
 		
-		default: ALUControlValues = 4'b1001;
+		default: ALUControlValues = 4'b1010;
 	endcase
 end
 
--- Control.v
+++ Control.v
@@ -23,36 +23,58 @@ module Control
 	output MemWrite,
 	output ALUSrc,
 	output RegWrite,
-	output [2:0]ALUOp
+	output Jump,
+	output Jal,
+  	output [3:0]ALUOp
+	
 );
 localparam R_Type = 0;
-localparam I_Type_ADDI = 6'h8;
+localparam I_Type_ADDI = 6'h08;
 localparam I_Type_ORI = 6'h0d;
+localparam I_Type_ANDI = 6'h0c;
+localparam I_Type_LUI =  6'h0f;
+localparam I_Type_SW =  6'h2b;
+localparam I_Type_LW =  6'h23;
+localparam I_Type_BEQ =  6'h04;
+localparam I_Type_BNE =  6'h05;
+localparam J_Type_JUMP=  6'h02;
+localparam J_Type_JAL =  6'h03;
 
 
-reg [10:0] ControlValues;
+reg [13:0] ControlValues;
 
 always@(OP) begin
 	casex(OP)
-		R_Type:       ControlValues= 11'b1_001_00_00_111;
-
-		
+		R_Type:       	ControlValues= 14'b001_001_00_00_1111; 
+		I_Type_ADDI:  	ControlValues= 14'b000_101_00_00_1000;
+		I_Type_ORI:	ControlValues= 14'b000_101_00_00_1010;
+		I_Type_ANDI:  	ControlValues= 14'b000_101_00_00_1100;
+		I_Type_LUI:	ControlValues= 14'b000_101_00_00_0010;
+		I_Type_LW:	ControlValues= 14'b000_111_10_00_1110;
+		I_Type_SW:	ControlValues= 14'b000_100_01_00_0110;	
+		I_Type_BEQ:	ControlValues= 14'b000_101_00_01_0100; 
+		I_Type_BNE:	ControlValues= 14'b000_101_00_10_0111;
+		J_Type_JUMP:  	ControlValues= 14'b010_000_00_00_0000;
+		J_Type_JAL:   	ControlValues= 14'b111_101_00_00_0000; 
 		
 		default:
-			ControlValues= 10'b0000000000;
+			ControlValues= 14'b000_000_00_00_0000;
 		endcase
 end	
-	
-assign RegDst = ControlValues[10];
-assign ALUSrc = ControlValues[9];
-assign MemtoReg = ControlValues[8];
-assign RegWrite = ControlValues[7];
-assign MemRead = ControlValues[6];
-assign MemWrite = ControlValues[5];
-assign BranchNE = ControlValues[4];
-assign BranchEQ = ControlValues[3];
-assign ALUOp = ControlValues[2:0];	
 
+assign Jal 	= ControlValues[13];
+assign Jump = ControlValues[12];
+assign RegDst = ControlValues[11];
+assign ALUSrc = ControlValues[10];
+assign MemtoReg = ControlValues[9];
+assign RegWrite = ControlValues[8];
+assign MemRead = ControlValues[7];
+assign MemWrite = ControlValues[6];
+assign BranchNE = ControlValues[5];
+assign BranchEQ = ControlValues[4];
+assign ALUOp = ControlValues[3:0];	
+
+ 
 endmodule
 //control//
 
